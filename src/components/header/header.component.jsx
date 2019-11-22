001af53869c20066e15380042e0e9393c2889435
rahul.kohli <kohlirahul23@gmail.com>
2019-11-23 04:29:28 +0530

    added redux to the project implemented mapStateToProps and mapDispatchToProps

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import './header.styles.scss';
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
 
 const Header = ({ currentUser }) => {
     return (
@@ -28,4 +29,8 @@ const Header = ({ currentUser }) => {
     )
 }
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
