057dfd75f659a3fb1e67644faba0194da59c6648
rahul.kohli <kohlirahul23@gmail.com>
2019-11-27 23:13:30 +0530

    added cart.utils to group similar items and assign quantity to each item when added to cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,7 +6,7 @@ import {ReactComponent as  ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 const CartIcon = ({ toggleCartHidden }) => (
         <div className="cart-icon" onClick={toggleCartHidden}>
-            <ShoppingIcon classname="shopping-icon" />
+            <ShoppingIcon className="shopping-icon" />
             <span className='item-count'>0</span>
         </div>
     )
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
+import CustomButton from "../custom-button/custom-button.component";
 
-const CollectionItem = ({ id, price, name, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+    const { price, name, imageUrl } = item;
     return (
         <div className="collection-item">
             <div className="image"
@@ -14,8 +18,13 @@ const CollectionItem = ({ id, price, name, imageUrl }) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton onClick={() => {addItem(item)}} inverted> Add to cart</CustomButton>
         </div>
     )
 }
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
