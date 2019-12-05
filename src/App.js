640d58762826b8d14b50b4d2e249131f16c0c16f
rahul.kohli <kohlirahul23@gmail.com>
2019-12-05 16:36:34 +0530

    collections page added with selector and preview-collections will now recieve array through selector

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path={'/'} component={HomePage} />
-          <Route exact path={'/shop'} component={ShopPage}/>
+          <Route path={'/shop'} component={ShopPage}/>
           <Route exact path={'/signIn'} render={() => this.props.currentUser ? (<Redirect to="" />) : (<SignInAndSignUpPage />)} />
           <Route exact path={'/checkout'} component={CheckoutPage} />
         </Switch>
